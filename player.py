6b59a9b9fb0fe10c0005d72739f0c93c8580d24d
gallo-json <gallojsantiago@gmail.com>
2020-05-08 10:21:43 -0500

    more buttons

--- player.py
+++ player.py
@@ -5,31 +5,43 @@ import threading
 import subprocess
 import glob
 
-f = open("password.txt", "r")
-p = f.read()
-f.close()
-
-button = 14
+pause_play = 14
+vol_up = 15
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.setup(pause_play, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(vol_up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 master, slave = os.openpty()
 
+os.system('mount /dev/sda1 ../../../../media/pi/usb')
+
 def pause():
     while True:
-        if GPIO.input(button) == False:
+        if GPIO.input(pause_play) == False:
                 os.write(slave, 's')
-                while GPIO.input(button) == False:
+                while GPIO.input(pause_play) == False:
                     time.sleep(0.2)
+
+def volume_up():
+    while True:
+        if GPIO.input(vol_up) == False:
+                os.write(slave, 'f')
+                while GPIO.input(vol_up) == False:
+                    time.sleep(0.2)
+
 try:
     player = subprocess.Popen(["mpg123", "-Z", "-C"] + glob.glob("../../../../media/pi/usb/*.mp3"), stdin=master)
     t1 = threading.Thread(target=pause, args=())
+    t2 = threading.Thread(target=volume_up, args=())
 
-    t1.start()        
+    t1.start()
+    t2.start()
+
     t1.join()
+    t2.join()
 except KeyboardInterrupt:
-    os.system('echo %s|sudo -S %s' % (p, 'whoami'))
     os.write(slave, 'q')
     GPIO.cleanup()
